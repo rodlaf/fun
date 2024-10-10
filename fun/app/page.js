e474d98d7660e56c02904a505ecb047903e25459
Rodney Lafuente Mercado <rodneyslafuente@gmail.com>
2024-10-10 03:25:26 -0400

    minor changes

--- fun/app/page.js
+++ fun/app/page.js
@@ -8,12 +8,14 @@ export default async function Home() {
 
     return (
         <StateProvider initialValue={state}>
-            <div className="flex flex-col items-center justify-center min-h-screen py-2">
-                <h1 className="text-xl font-bold text-center mb-4">Welcome to Fun!</h1>
+            <div className="flex flex-col items-center justify-center min-h-screen p-2 bg-gray-100">
+                <h1 className="text-2xl font-bold text-center mb-6 text-blue-600">Welcome to ColorFun!</h1>
+                <div className="text-lg mb-2">These buttons are a shared state of all users!</div>
+                <div className="text-md mb-4">This is a demo of <a href="https://github.com/rodlaf/fun" className="text-blue-500 underline">React Server Components and WebSockets</a>.</div>
                 <div className="grid grid-cols-2 gap-4">
                     {
                         data.state.map((item, index) => {
-                            return <div key={index}><Rotator index={index} initialValue={item} /></div>
+                            return <div key={index} className="p-2 bg-white shadow rounded"><Rotator index={index} initialValue={item} /></div>
                         })
                     }
                 </div>
